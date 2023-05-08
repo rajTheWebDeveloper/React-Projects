30d43c2fa4a12a09b5a8922fc3a896390b8b2b3e
Raj <rockingraj8247@gmail.com>
2023-05-08 09:42:07 +0530

    First Save

---
+++ react-projects/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React after a long time</h1>
+    </div>
+  );
+}
+
+export default App;
